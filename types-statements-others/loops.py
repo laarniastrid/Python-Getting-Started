d929a23de381008806a43ceec6be9999462b108d
laarniastrid <laarni.astrid@gmail.com>
2017-08-08 00:17:46 -0600

    practice

--- types-statements-others/loops.py
+++ types-statements-others/loops.py
@@ -1,13 +1,27 @@
+# basic loops
 student_names = ['mark', 'katarina', 'jessica']
 
 for name in student_names:
     print('student name is {0}'.format(name))
 
-# ----------
 
+# loops with range
 x = 0
 for index in range(10):
     x += 10
     print('the value of X is {0}'.format(x))
 
-#
+# loops with range not starting with 0
+# 2 arguments [start, end]
+for index in range(5, 10):
+    print('the value iis {0}'. format(index))
+
+for index in range(5, 10):
+    index += 2
+    print('the value is {0}'. format(index))
+
+
+# loops with range not starting with 0
+# 3 arguments [start, end, increment[
+for index in range(5, 30, 3):
+    print('the value is {0}'.format(index))
