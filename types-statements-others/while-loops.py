294631530d3428d0aceb82d152d67331e37f8592
laarniastrid <laarni.astrid@gmail.com>
2017-08-08 21:22:28 -0600

    practice

---
+++ types-statements-others/while-loops.py
@@ -0,0 +1,15 @@
+# while loops
+x = 0
+
+while x < 10:
+    print('count is {0}'.format(x))
+    x += 1
+
+
+# # will cause an infinite loop
+# num = 10
+# while True:
+#     if num == 42:
+#         break
+#     print('hello world')
+
