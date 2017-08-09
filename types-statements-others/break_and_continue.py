f29e860d6f463ea8b9232803d1c490fe19871d30
laarniastrid <laarni.astrid@gmail.com>
2017-08-08 20:27:28 -0600

    practice

---
+++ types-statements-others/break_and_continue.py
@@ -0,0 +1,19 @@
+#
+student_names = [
+    'james',
+    'katarina',
+    'jessica',
+    'mark',
+    'bort',
+    'frank grimes',
+    'max power'
+]
+
+# print(student_names)
+
+for name in student_names:
+    if name == 'bort':
+        continue
+        # print('found him! ' + name)
+    print('current testing ' + name)
+
