9122fdd085e40f78ff134f883f0c47ddd4d3f6a0
laarniastrid <laarni.astrid@gmail.com>
2017-08-08 22:32:50 -0600

    practice

--- functions-files-yield-lambda/functions.py
+++ functions-files-yield-lambda/functions.py
@@ -1,24 +1,33 @@
 # functions
-print('hello world')
-str(3)  # '3'
-int('15')  # 15
-username = input('enter the user\'s name: ')
+# print('hello world')
+# str(3)  # '3'
+# int('15')  # 15
+# username = input('enter the user\'s name: ')
 
 #
 students = []
+
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
         students_titlecase = student.title()
     return students_titlecase
 
-def print_students_title():
-    get_students_titlecase = []
-    for student in students:
-        get_students_titlecase = student.title()
-    print(print_students_title)
+# # similar to get_students_titlecase function
+# def print_students_title():
+#     students_titlecase = []
+#     for student in students:
+#         students_titlecase = student.title()
+#     print(print_students_title)
+
+def print_students_titlecase():
+    students_titlecase = get_students_titlecase()
+
 
 def add_student(name):
     students.append(name)
 
 student_list = get_students_titlecase()
+
+add_student('mark')
+print(students)
