f637555b93627cca4dd774e553553885d43bcf90
laarniastrid <laarni.astrid@gmail.com>
2017-08-08 22:08:06 -0600

    practice

--- types-statements-others/exceptions.py
+++ types-statements-others/exceptions.py
@@ -5,9 +5,14 @@ student = {
     'feedback': None
 }
 
+student['last_name'] = 'kowalski'
+
 try:
     last_name = student['last_name']
+    numbered_last_name = 3 + last_name
 except KeyError:
     print('error finding last_name')
+except TypeError:
+    print('i can\'t add these two together!')
 
 print('this code executes')
