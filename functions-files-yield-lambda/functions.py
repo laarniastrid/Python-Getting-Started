068e3396680692213925928bd6cd752713511b13
laarniastrid <laarni.astrid@gmail.com>
2017-08-10 23:59:14 -0600

    practice

--- functions-files-yield-lambda/functions.py
+++ functions-files-yield-lambda/functions.py
@@ -22,7 +22,7 @@ def get_students_titlecase():
 
 def print_students_titlecase():
     students_titlecase = get_students_titlecase()
-
+    print(students_titlecase)
 
 def add_student(name):
     students.append(name)
