c4ba48f067390ee201528a9b9fb1966b83bf5aa4
laarniastrid <laarni.astrid@gmail.com>
2017-08-18 00:54:59 -0600

    practice

--- functions-files-yield-lambda/opening-reading-writing-files.py
+++ functions-files-yield-lambda/opening-reading-writing-files.py
@@ -4,7 +4,7 @@ students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase = student['name'].title()
+        students_titlecase.append(student['name'].title())
     return students_titlecase
 
 def print_students_titlecase():
@@ -18,11 +18,32 @@ def add_student(name, student_id=332):
     }
     students.append(student)
 
-student_list = get_students_titlecase()
+def save_file(student):
+    try:
+        f = open('students.txt', 'a')
+        f.write(student + '\n')
+        f.close()
+    except Exception:
+        print('could not save file')
+
+def read_file():
+    try:
+        f = open('students.txt', 'r')
+        for student in f.readlines():
+            add_student(student)
+        f.close()
+    except Exception:
+        print('could not read file')
+
+# student_list = get_students_titlecase()
+
+read_file()
+print_students_titlecase()
 
 student_name = input('enter student name : ')
 student_id = input('enter student id : ')
 
-add_student(student_nama, student_id)
+add_student(student_name, student_id)
+save_file(student_name)
 
-print_students_titlecase()
+# print_students_titlecase()
